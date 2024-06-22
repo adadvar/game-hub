378e59d8f0cc95ddab6eabd2a1f65b697676cafb
adadvar <a.dadvar_71@yahoo.com>
2024-06-22 06:59:37 +0330

    impliment dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,26 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav" bg="coral">
-        <Navbar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" bg="blue">
-          Aside
-        </GridItem>
-      </Show>
-      <GridItem area="main" bg="red">
-        Main
-      </GridItem>
-    </Grid>
-  );
+	return (
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`,
+			}}
+		>
+			<GridItem area="nav">
+				<Navbar />
+			</GridItem>
+			<Show above="lg">
+				<GridItem area="aside" bg="blue">
+					Aside
+				</GridItem>
+			</Show>
+			<GridItem area="main" bg="red">
+				Main
+			</GridItem>
+		</Grid>
+	);
 }
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 
 const Navbar = () => {
-  return (
-    <HStack>
-      <Image src={logo} />
-    </HStack>
-  );
+	return (
+		<HStack>
+			<Image src={logo} boxSize={"60px"} />
+			<Text>Navbar</Text>
+		</HStack>
+	);
 };
 
 export default Navbar;
