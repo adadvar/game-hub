5a9ad30d8f0419cd5825496f61c2492c6f3795d2
adadvar <a.dadvar_71@yahoo.com>
2024-06-29 09:50:24 +0330

    simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { GameQuery } from "../App";
 import { useInfiniteQuery } from '@tanstack/react-query'
 import APIClient, { FetchResponse } from '../services/api-client'
 import { Platform } from './usePlatforms'
+import ms from "ms";
 
 const apiClient = new APIClient<Game>('/games')
 
@@ -25,11 +26,11 @@ const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>,
       page: pageParam,
     }
   }),
-  staleTime: 24 * 60 * 60 * 1000,
   getNextPageParam: (lastPage, allPages) => {
     return lastPage.next ? allPages.length + 1 : undefined;
   },
-  initialPageParam: 1
+  initialPageParam: 1,
+  staleTime: ms('24h'),
 })
 
 
