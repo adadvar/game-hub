f7f366b1c4622a6099cf793b50e62aae59d62653
adadvar <a.dadvar_71>
2024-07-02 18:10:09 +0330

    Refactor entity interfaces

--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -1,5 +1,5 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import CriticStore from "./CriticStore";
 import DefinitionItem from "./DefinitionItem";
 
